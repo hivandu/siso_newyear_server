dbf6b4ae8f67295aaa13a2ed759a46464645dfdc
Hivan Du <doo@hivan.me>
2016-12-18 15:14:30 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,83 @@
+var express = require('express'),
+    app = express(),
+    MongoClient = require('mongodb');
+var server = require("http").Server(app);
+var socket = require("socket.io").listen(server);
+
+server.listen(1246);
+console.log('Server running at 120.0.0.1:1246');
+
+//  connect to mongo
+MongoClient.connect('mongodb://localhost:27017/siso_newyear', function (err, db) {
+    if (err) throw err;
+	console.log('connected to mongo..');
+
+    /** socket on */
+    // Run the server
+    init();
+
+    // Initialization
+    function init() {
+        setEventHandlers();
+    }
+
+    // Event handlers
+    function setEventHandlers() {
+        // Socket.IO
+        socket.on("connection", onSocketConnection);
+    }
+
+
+    // New socket connection
+    function onSocketConnection(socket) {
+        console.log('Someone comes here!!!!');
+
+        //  on saveImage
+        socket.on('saveImage', onSaveImageHandler);
+
+        function onSaveImageHandler (data) {
+            var that = this;
+
+            console.log('someone save he\'s image.');
+
+            //  update visitedNumber
+            db.collection('visitedNumber').update({"visitedNumber": {$exists: 1}}, {$inc: {"visitedNumber": 1}});
+            db.collection('numberOfSavedImages:').update({"numberOfSavedImages:": {$exists: 1}}, {$inc: {"numberOfSavedImages:": 1}});
+            db.collection('saveImages').update({'saveImages': {$exists:1}}, {$set: data});
+
+            //  send result to client
+            that.emit('saveImageResult', { result: true });
+        }
+
+        //  on get image
+        socket.on('getImage', onGetImageHandler);
+
+        function onGetImageHandler (imgID) {
+            var that = this;
+
+            console.log('some request image.');
+
+            // query image
+            var queryObj = {};
+            queryObj[imgID] = {$exists:1};
+
+            var productObj = { _id:0 };
+            productObj[imgID] = 1;
+
+            db.collection('saveImages').find(queryObj, productObj).toArray(function (err, docs) {
+                //console.log(docs);
+
+                try {
+                    if (docs) {
+                        //  return image
+                        console.log('send image back to client.');
+                        that.emit('returnImage', {result: docs[0][imgID]});
+                    }
+                } catch (err) {
+                    that.emit('returnImage', {result: false});
+                }
+            });
+
+        }
+    }
+});
